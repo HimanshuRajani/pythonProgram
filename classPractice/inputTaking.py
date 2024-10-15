738d2f4f2cbf769c2d471da19d57e9313b583374
HimanshuRajani <himanshurajani31@gmail.com>
2024-10-15 14:47:55 +0530

    new

--- classPractice/inputTaking.py
+++ classPractice/inputTaking.py
@@ -8,3 +8,18 @@ first.age=input("enter the user age :- ")
 first.city=input("enter the city where the user leave:- ")
 
 print("name:-",first.name,"|| age :-",first.age,"|| city:-",first.city)
+
+
+class second():
+    name=""
+    dob=""
+    gender=""
+    def  __init__(self,name,dob,gender):
+        self.name=name
+        self.dob=dob
+        self.gendr=gender
+
+mine=second("sakshi","14-Aug-2001","female")
+
+print(mine.name,mine.dob,mine.gender)
+
