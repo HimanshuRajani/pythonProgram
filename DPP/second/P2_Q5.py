cdff3fd451a59148889ef95df8dd31f9c4f2cd82
HimanshuRajani <himanshurajani31@gmail.com>
2024-10-20 13:16:00 +0530

    changes

--- DPP/second/P2_Q5.py
+++ DPP/second/P2_Q5.py
@@ -1,7 +1,14 @@
 string=input("enter the full string you have:- ")
 sub_string=input("enter the part of string you want to find:- ")
 
-if sub_string in string:
-    print("the sub string is found in the main string")
-else:
-    print("string not found")
+# if sub_string in string:
+#     print("the sub string is found in the main string")
+# else:
+#     print("string not found")
+
+def find_substring(string,sub_string):
+    index =string.find(sub_string)
+    return index
+
+print(find_substring(string,sub_string))
+
--- DPP/second/P2_Q6.py
+++ DPP/second/P2_Q6.py
@@ -1,8 +1,13 @@
 string = "hie i am here i am there i will be everywhere"
-replace = "m"
+replace ='m'
 replace_with = "you"
 
-for replace in string:
-    new_string = string.replace(replace,replace_with)
+# for replace in string:
+#     new_string = string.replace(replace,replace_with)
 
-print(new_string)
+# print(new_string)
+def replace_word(string, replace, replace_with):
+    new_string = string.replace(replace, replace_with)
+    print (new_string)
+
+replace_word(string, replace, replace_with)
