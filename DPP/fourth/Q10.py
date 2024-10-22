f58007adc57413de65e8f6f9b018fed85c8a19a7
HimanshuRajani <himanshurajani31@gmail.com>
2024-10-22 23:39:47 +0530

    just nohthing

--- DPP/fourth/Q10.py
+++ DPP/fourth/Q10.py
@@ -1,2 +1,10 @@
-# def monthly_payment(principal,rate,years):
-    
+def monthly_payment(principal,rate,years):
+    payment = principal*((rate*(1+rate)**years)/(((1+rate)**years)-1))
+
+    return payment
+
+princ = int(input("Enter the principle amount:- "))
+rate = int(input("Enter the rate :- "))
+years = int(input("Enter the number of years:- "))
+
+print("Monthly payment is:- ",int(monthly_payment(princ,rate/12,years*12)))
