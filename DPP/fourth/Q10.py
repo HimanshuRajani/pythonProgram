f4a2f00ee22a1a70f9089e92c3b2e6e5838a25bb
HimanshuRajani <himanshurajani31@gmail.com>
2024-10-24 12:53:57 +0530

    final question of DPP-4

--- DPP/fourth/Q10.py
+++ DPP/fourth/Q10.py
@@ -1,10 +1,22 @@
-def monthly_payment(principal,rate,years):
-    payment = principal*((rate*(1+rate)**years)/(((1+rate)**years)-1))
+# def monthly_payment(principal,rate,years):
+#     payment = principal*((rate*(1+rate)**years)/(((1+rate)**years)-1))
 
-    return payment
+#     return payment
 
-princ = int(input("Enter the principle amount:- "))
-rate = int(input("Enter the rate :- "))
-years = int(input("Enter the number of years:- "))
+# princ = int(input("Enter the principle amount:- "))
+# rate = int(input("Enter the rate :- "))
+# years = int(input("Enter the number of years:- "))
 
-print("Monthly payment is:- ",int(monthly_payment(princ,rate/12,years*12)))
+# print("Monthly payment is:- ",int(monthly_payment(princ,rate/12,years*12))
+def monthly_payment(principal, rate, years):
+    monthly_rate = rate / 100 / 12
+    total_payments = years * 12
+    monthly_payment = principal * (monthly_rate * (1 + monthly_rate)**total_payments) / ((1 + monthly_rate)**total_payments - 1)
+    return monthly_payment
+
+principal = int(input("Enter the loan amount you want to lend :- "))
+rate = int(input("the rate on which you got the principal amount :- "))
+years = int(input("For hoe much years :- "))
+
+payment = monthly_payment(principal, rate, years)
+print(f"Monthly mortgage payment: {payment:.2f} rs")
